e1f51864ba6692f0047e921773cad80c2af5cfb2
Ratibor Siryi <siryi.ra@phystech.edu>
2023-09-04 22:28:03 +0300

    shell config reorganization

--- sync.sh
+++ sync.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
 stow --target=$HOME --ignore="sync.sh" .
+
+[ ! -e "${HOME}/.config/rofi/themes" ] &&
+    ln -s "${ROFI_THEME_DIR}" "${HOME}/.config/rofi/themes"
+[ ! -e "${HOME}/.Xresources" ] &&
+    ln -s "${XRESOURCES_CONFIG}" "${HOME}/.Xresources"
